94931bc1309ebe8f7c7c6ea268d33a6556653967
Formagin <austin.formagin117@gmail.com>
2018-05-29 14:11:44 -0400

    Initial commit - No Code Added

---
+++ src/com/hackuccts/Main.java
@@ -0,0 +1,14 @@
+package com.hackuccts;
+
+//Hack UCC project for the ThirdSpare team
+//Programmers: Austin Formagin, Michael Willburger, Nathan Luth
+//Date of Competition: 09.06.18
+//Time of Start: 0000
+//Time of Finish: 0000
+
+public class Main {
+
+    public static void main(String[] args) {
+	//Let the coding begin!
+    }
+}
